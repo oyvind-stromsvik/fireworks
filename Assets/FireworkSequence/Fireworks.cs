1069d99b1d08e9bbbfabc09fda9b066f382f9208
Øyvind Strømsvik <oyvind@nymedia.no>
2020-04-19 08:35:23 +0200

    Add project files

---
+++ Assets/FireworkSequence/Fireworks.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireworks : MonoBehaviour {
+
+	// A list of the sounds to play when launching a rocket.
+	public AudioClip[] mortarSounds;
+
+	// A list of the sounds to play when a rocket explodes.
+	public AudioClip[] explodeSounds;
+
+	// A list of all our mortar tubes.
+	public ParticleSystem[] mortars;
+	
+	private ParticleSystem.Particle[] particles = new ParticleSystem.Particle[1000];
+	private Vector3 listenerPosition;
+	private float speedOfSound = 343.2f;
+	private AudioClip clip;
+	private float distance;
+	
+	void Start () {
+		listenerPosition = Camera.main.transform.position;
+	}
+	
+	void Update() {
+		foreach (ParticleSystem mortar in mortars) {
+			int length = mortar.GetParticles(particles);
+			int i = 0;
+			while (i < length) {
+				if (particles[i].remainingLifetime < Time.deltaTime) {
+					clip = explodeSounds[Random.Range(0, explodeSounds.Length)];
+					distance = Vector3.Distance(particles[i].position, listenerPosition);
+					StartCoroutine(PlayDelayedSound(clip, distance, "explosion"));
+				}
+				i++;
+			}
+		}
+	}
+
+	IEnumerator PlayDelayedSound(AudioClip clip, float distance, string type) {
+		float waitTime = distance / speedOfSound;
+        
+        yield return new WaitForSeconds(waitTime);
+
+		if (type == "mortar") {
+			GetComponent<AudioSource>().volume = 0.6f;
+        	GetComponent<AudioSource>().pitch = Random.Range(0.6f, 0.8f);
+		}
+		else {
+			GetComponent<AudioSource>().pitch = Random.Range(0.5f, 1.5f);
+		}
+        
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+
+	// Launches a rocket from the given mortar tube.
+	// We call this method from the attached animation clip.
+	// This lets us compose a fireworks sequence using the animation view.
+	void LaunchRocket (int mortarTube) {
+		mortars[mortarTube].Emit(1);
+		distance = Vector3.Distance(mortars[mortarTube].transform.position, listenerPosition);
+		clip = mortarSounds[Random.Range(0, mortarSounds.Length)];
+		StartCoroutine(PlayDelayedSound(clip, distance, "mortar"));
+    }
+}
---
+++ Assets/README.txt
@@ -0,0 +1,31 @@
+___________
+
+ FIREWORKS
+___________
+
+
+ABOUT
+=====
+Made by Øyvind Strømsvik (TwiiK)
+http://twiik.net/projects/shuriken-fireworks
+
+LICENSE/COPYRIGHT
+=================
+The sound effects are from freesound.org and all credit go to their respective 
+creators. The links are listed below. Do whatever you wish with the rest of the
+project.
+
+KNOWN ISSUES
+============
+There are no known issues at this time.
+
+ROADMAP
+=======
+There are no planned features at this time.
+
+ADDITIONAL INFORMATION
+======================
+Sound effects used:
+http://www.freesound.org/people/unfa/sounds/209310
+http://www.freesound.org/people/Robinhood76/sounds/65731
+http://www.freesound.org/people/Heigh-hoo/sounds/21410
